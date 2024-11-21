cbe686f6140a588bd008b80d4b6eeba1956f4141
Anna Poltavchenko <anna.poltavchenko@bystrov.agency>
2024-11-21 14:18:49 +0100

    Add styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,9 @@
 import 'the-new-css-reset/css/reset.css';
 import '../styles/style.scss';
 
-const setupCounter = (element) => {
-  let counter = 0;
-  const counterElement = element;
-  const setCounter = (count) => {
-    counter = count;
-    counterElement.innerHTML = `count is ${counter}`;
-  };
-  element.addEventListener('click', () => setCounter(counter + 1));
-  setCounter(0);
+const hello = () => {
+  // eslint-disable-next-line no-console
+  console.log('Hello World');
 };
 
-setupCounter(document.querySelector('#counter'));
-
-document.querySelector('.tags').innerHTML = dependencies.map((dependency) => `<p>${dependency}</p>`).join('');
+hello();
